ca529711ebc81cb7afcf050dbda5fc38a784a7c8
taniaorozcoh <taniaorozhernan@gmail.com>
2026-09-04 16:09:43 -0600

    update

---
+++ refugio_huellas/entities/GatoEntity.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "EmpleadoEntity.h"
+
+using namespace std;
+
+enum class Estado {
+    NO_ADOPTADO = 0,
+    ADOPTADO = 1
+};
+
+class GatoEntity {
+public:	
+	int id;
+    string nombre;
+    int edad;
+    string test;
+    string indoor;
+    string diagnostico;
+    Estado estado;
+	string telefono;
+	string correo
+
+
+
+
+private:
+	void setNombre(string nombre) {
+		this->nombre = nombre;
+	}
+};
